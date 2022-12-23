fc37337f86a07f3afe702888a79db890e36b564a
Vlad Sheiko <sheiko.vlad@gmail.com>
2022-12-23 10:28:33 +0200

    разукрасил и сделал поле больше

--- DogGameByKrasheninnikovIllja/MainSourse.cpp
+++ DogGameByKrasheninnikovIllja/MainSourse.cpp
@@ -5,18 +5,19 @@
 #include <ctime>
 #include <vector>
 #include <conio.h>
+#include <map>
 
 using namespace std;
 
 enum chars {
 	dogS = '@',
 	borderS = '#',
-	voidS = ' ',
+	voidS = '.',
 	exitS = 'O'
 };
 
 
-const int FIELD_WIDTH = 10;
+const int FIELD_WIDTH = 24;
 const int FIELD_HEIGHT = 12;
 const int block_freq = 25;
 
@@ -25,30 +26,40 @@ struct Cords {
 	int x;
 	int y;
 
-	Cords(int x, int y): x(x), y(y) {}
+	Cords(int x, int y) : x(x), y(y) {}
+
+	Cords operator + (Cords b) {
+		return Cords(this->x + b.x, this->y + b.y);
+	}
 };
 
-
-char field_mash[FIELD_HEIGHT][FIELD_WIDTH];
+ char field_mash[FIELD_HEIGHT][FIELD_WIDTH];
+//char field_mash[FIELD_HEIGHT][FIELD_WIDTH];
 
 Cords dog(0, 0);
-Cords door(0, 0);
 Cords vectorMove(0, 0);
 
 void field_gen() {
 	for (int i = 0; i < FIELD_HEIGHT; i++) {
 		for (int j = 0; j < FIELD_WIDTH; j++) {
 			int rand_num = rand() % 100;
-			if (rand_num < block_freq)
+			if (rand_num < block_freq) {
 				field_mash[i][j] = borderS;
+			}
 			else
+			{
 				field_mash[i][j] = voidS;
+			}
 		}
 	}
-	door.x = rand() % FIELD_WIDTH;
-	door.y = rand() % FIELD_HEIGHT;
-	field_mash[door.y][door.x] = exitS;
-
+	int x, y;
+	do {
+		x = rand() % FIELD_WIDTH;
+		y = rand() % FIELD_HEIGHT;
+	} while (field_mash[y][x] == borderS);
+	//field_mash[door.y][door.x] = exitS;
+	//додаю вихід в map
+	field_mash[y][x]= exitS;
 }
 void draw() {
 	//перенос курсора на початок сторінки
@@ -64,15 +75,29 @@ void draw() {
 			if (i == dog.y && j == dog.x)
 			{
 				//змінення кольору щоб намалювати собаку
-				SetConsoleTextAttribute(hStdOut, BACKGROUND_BLUE | FOREGROUND_GREEN);
-				cout << (char) dogS;
+				SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE | FOREGROUND_GREEN);
+				cout << (char)dogS;
 				//повернення кольору
 				SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
 			}
 			else {
-				cout << field_mash[i][j];
+				char s = field_mash[i][j];
+				switch (s)
+				{
+				case borderS:
+					SetConsoleTextAttribute(hStdOut, FOREGROUND_GREEN);
+					break;
+				case exitS:
+					SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE| FOREGROUND_INTENSITY);
+					break;
+				default:
+					SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+					break;
+				}
+				cout << s;
 			}
 		}
+		SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
 		cout << endl;
 	}
 }
@@ -81,7 +106,7 @@ void dog_place() {
 	do {
 		dog.x = rand() % FIELD_WIDTH;
 		dog.y = rand() % FIELD_HEIGHT;
-	} while ((field_mash[dog.y][dog.x]) == borderS); //щоб собака не з'являвся в решітці
+	} while (field_mash[dog.y][dog.x] == borderS); //щоб собака не з'являвся в решітці
 }
 
 void generate() {
@@ -90,7 +115,7 @@ void generate() {
 }
 
 bool game_is_over() {
-	return (dog.y == door.y && dog.x == door.x);
+	return (field_mash[dog.y][dog.x] == exitS);
 }
 
 void get_input() {
@@ -117,37 +142,37 @@ void get_input() {
 	}
 }
 
-bool is_walkable(int X, int Y) {
-	return field_mash[Y][X] != borderS;
+bool is_walkable(Cords newCord) {
+	return field_mash[newCord.y][newCord.x] != borderS;
+	//return field_mash[Y][X] != borderS;
 }
 
-bool can_go_to(int newX, int newY) {
-	if (newX < 0 || newX >= FIELD_WIDTH || newY < 0 || newY >= FIELD_HEIGHT)
-		return false;
-	if (!is_walkable(newX, newY))
-		return false;
-	else
-		return true;
+bool is_dont_end_pole(Cords cord) {
+	return (cord.x >= 0 && cord.x < FIELD_WIDTH&& cord.y >= 0 && cord.y < FIELD_HEIGHT);
 }
 
-void go_to(int newX, int newY) {
-	dog = Cords(newX,newY);
+bool can_go_to(Cords newCord) {
+	return (is_walkable(newCord) && is_dont_end_pole(newCord));
 }
 
-void try_to_go(int new_X, int new_Y) {
-	if (can_go_to(new_X, new_Y))
-		go_to(new_X, new_Y);
+void go_to(Cords newCord) {
+	dog = newCord;
+}
+
+void try_to_go(Cords newCord) {
+	if (can_go_to(newCord))
+		go_to(newCord);
 }
 
 void logic() {
-	try_to_go(dog.x + vectorMove.x, dog.y + vectorMove.y);
+	try_to_go(dog + vectorMove);
 }
 
 
 int main() {
-	//srand((unsigned int) time(NULL));
+	srand((unsigned int) time(NULL));
 	//тимчасово один сід для тестів
-	srand(10000000);
+	//srand(10000000);
 	generate();
 	draw();
 	while (!game_is_over()) {
